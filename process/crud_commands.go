c8a465051d44175712ddeb237b4fc98c8fc170ce
xmp-er <107166230+xmp-er@users.noreply.github.com>
2024-02-28 03:51:06 +0530

    GET implementation

--- process/crud_commands.go
+++ process/crud_commands.go
@@ -1,11 +1,14 @@
 package process
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
 
-func crud(str []string) { //expectation is that the incoming command must be SET,GET, OR DEL
+//1 stands for successful op, 0/nil for failed op
+
+func crud(str []string) int { //expectation is that the incoming command must be SET,GET, OR DEL
 	if str[0] == "SET" {
 		op_set(str)
 	} else if str[0] == "GET" {
@@ -15,15 +18,24 @@ func crud(str []string) { //expectation is that the incoming command must be SET
 	}
 }
 
-func op_set(str []string) { //SET operation implementation, assumption all input correct
+func op_set(str []string) int { //SET operation implementation, assumption all input correct
 	k := str[1]
 	v := str[2]
 	temp_v, err := strconv.ParseFloat(v, 64)
 	if err != nil {
 		//it means we have a float64 and we can assign it to Map_int
 		Map_int[k] = temp_v
-		return
+		return 1
 	}
 	//now the remaining can only be the string value so adding it in Map_string
 	Map_string[k] = strings.Trim(v, "\"")
+	return 1
+}
+
+func op_get(str []string) string { //GET implementation, assuming the input to be correct
+	k := str[1]
+	if _, ok := Map_string[k]; !ok {
+		return fmt.Sprintf("%f", Map_int[k])
+	}
+	return Map_string[k]
 }
